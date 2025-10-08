5c9a54b2492dcd20297a5f8c5cbed2c57684a86c
JoseEM26 <joseangelespinozamorales@gmail.com>
2025-10-07 21:24:39 -0500

    'usuarios'

--- CompuTronica/app/src/main/java/com/example/computronica/Usuarios.kt
+++ CompuTronica/app/src/main/java/com/example/computronica/Usuarios.kt
@@ -10,7 +10,7 @@ class Usuarios : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_usuarios)
+        setContentView(R.layout.activity_perfil)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
