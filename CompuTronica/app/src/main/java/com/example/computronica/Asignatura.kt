568c710f780cc0b77dd1690e379438dc645c8d95
JoseEM26 <joseangelespinozamorales@gmail.com>
2025-10-16 17:24:19 -0500

    diseñoAsignatura

--- CompuTronica/app/src/main/java/com/example/computronica/Asignatura.kt
+++ CompuTronica/app/src/main/java/com/example/computronica/Asignatura.kt
@@ -11,7 +11,7 @@ class Asignatura : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_asignatura)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.scroll)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
